6fd4e9a09ce95089d162ab4089a2d95d2daf0063
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2022-11-07 16:49:00 +0300

    Third commit

--- src/index.js
+++ src/index.js
@@ -8,31 +8,40 @@ const timeSession = document.querySelector('.js-session');
 const timeBreak = document.querySelector('.js-break');
 const outputTimeMinutes = document.querySelector('.container__time-minut');
 const outputTimeSecond = document.querySelector('.container__time-second');
-const timerStart = document.querySelector('.container__start');
-const timerReset = document.querySelector('.container__reset');
+//const timerStart = document.querySelector('.container__start');
+//const timerReset = document.querySelector('.container__reset');
+const btn = document.querySelector('.container__button');
 const title = document.querySelector('.container__sub-title');
+
+if (JSON.parse(localStorage.getItem('minutesBreak')) == null
+    || JSON.parse(localStorage.getItem('minutesSession')) == null) {
+        localStorage.setItem("minutesBreak", JSON.stringify(5));
+        localStorage.setItem("minutesSession", JSON.stringify(25));
+    }
 
 timeBreak.innerHTML = JSON.parse(localStorage.getItem('minutesBreak'));
 timeSession.innerHTML = JSON.parse(localStorage.getItem('minutesSession'))
 outputTimeMinutes.innerHTML = JSON.parse(localStorage.getItem('minutesSession'))
 
 let second = 59;
-let minutes = Number(timeSession.textContent) - 1;
+let minutes = Number(JSON.parse(localStorage.getItem('minutesSession'))) - 1;
 let countIteration = 0;
 let startTimer = false;
+let isTimerActive = false;
+let timerActive = '';
 
 // Настрайка времени break, кнопки + и -
 
 ChangeTimeBreak.addEventListener('click', (event) => {
     const { target } = event;
     let timeMinutes = Number(timeBreak.textContent);
-    if(target.textContent === '+') {
+    if (target.textContent === '+') {
         timeMinutes += 1;
         timeBreak.innerHTML = timeMinutes;
         localStorage.setItem("minutesBreak", JSON.stringify(timeBreak.textContent));
     }
 
-    if(target.textContent === '-') {
+    if (target.textContent === '-') {
         timeMinutes -= 1;
         timeBreak.innerHTML = timeMinutes;
         localStorage.setItem("minutesBreak", JSON.stringify(timeBreak.textContent));
@@ -43,14 +52,14 @@ ChangeTimeBreak.addEventListener('click', (event) => {
 
 ChangeTimeSession.addEventListener('click', (event) => {
     const { target } = event;
-    if(target.textContent === '+') {
+    if (target.textContent === '+') {
         minutes += 1;
         timeSession.innerHTML = minutes + 1;
         outputTimeMinutes.innerHTML = minutes + 1;
         localStorage.setItem("minutesSession", JSON.stringify(timeSession.textContent));
     }
 
-    if(target.textContent === '-') {
+    if (target.textContent === '-') {
         minutes -= 1;
         timeSession.innerHTML = minutes + 1;
         outputTimeMinutes.innerHTML = minutes + 1;
@@ -60,67 +69,50 @@ ChangeTimeSession.addEventListener('click', (event) => {
 
 // Вывод времени
 
-function timer() {
-    const countSecond = setInterval(() => {
-        if(String(minutes).length != 2) {
-            while(String(minutes).length != 2) {
-                minutes = '0' + minutes;
-            }
-        }
-        
-        if(String(second).length != 2) {
-            while(String(second).length != 2) {
-                second = '0' + second;
-            }
-        }
-        
-        outputTimeMinutes.innerHTML = (`${minutes}`);
-        outputTimeSecond.innerHTML = (`${second}`);
+const timer = () => {
+    if (String(minutes).length != 2) {
+        minutes = '0' + minutes;
+    }
     
-        second -= 1;
-        if(second < 0) {
-            minutes -= 1;
-            second = 59;
-            if(minutes < 0 ) {
-                countIteration++
-                if(countIteration % 2 === 0) {
-                    minutes = Number(timeSession.textContent) - 1;
-                    second = 59;
-                    title.innerHTML = 'session';
-                } else {
-                    minutes = Number(timeBreak.textContent) - 1;
-                    second = 59;
-                    title.innerHTML = 'break';
-                }
-            }
-        }
+    if (String(second).length != 2) {
+        second = '0' + second;
+    }
     
-        timerReset.addEventListener('click', () => {
-            clearInterval(countSecond);
-            outputTimeMinutes.innerHTML = timeSession.innerHTML
-            outputTimeSecond.innerHTML = '00';
-            title.innerHTML = 'session'
-            second = 59
-            startTimer = false;
-        })
-    
-    }, 1000);
-}
+    outputTimeMinutes.innerHTML = (`${minutes}`);
+    outputTimeSecond.innerHTML = (`${second}`);
 
-
-// Функция запуска звукового сигнала
-
-//function soundEnd() {
-//    const audio = new Audio();
-//    audio.src = 'dzin.MP3';
-//    audio.autoplay = true;
-//}
+    second -= 1;
+    if (second < 0) {
+        minutes -= 1;
+        second = 59;
+        if (minutes < 0 ) {
+            countIteration++
+            if (countIteration % 2 === 0) {
+                minutes = Number(timeSession.textContent) - 1;
+                second = 59;
+                title.innerHTML = 'session';
+            } else {
+                minutes = Number(timeBreak.textContent) - 1;
+                second = 59;
+                title.innerHTML = 'break';
+            };
+        };
+    };
+};
 
 // Старт таймера
 
-timerStart.addEventListener('click', () => {
-    if(!startTimer) {
+btn.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target.textContent === 'Start' && !startTimer) {
         startTimer = true;
-        timer()
-    }
-})
+        timerActive = setInterval(timer, 1000);
+    } else if (target.textContent === 'Reset') {
+        clearInterval(timerActive);
+        outputTimeMinutes.innerHTML = timeSession.innerHTML
+        outputTimeSecond.innerHTML = '00';
+        title.innerHTML = 'session'
+        second = 59
+        startTimer = false;
+    };
+});
